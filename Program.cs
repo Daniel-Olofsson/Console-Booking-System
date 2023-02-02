c58430b056c7fc8e38d387b35a0d48045e57c7a8
Daniel-Olofsson <daniel-olofsson@hotmail.se>
2023-02-02 12:37:59 +0100

    fixed the correct time and date

--- Program.cs
+++ Program.cs
@@ -13,15 +13,15 @@ namespace BookingSystem
             var inputHandler = new HandleBooking();
             var userOne = new Person("Daniel", "Olofsson", userID++);
             var userTwo = new Person("Hades", "kattson", userID++);
-            DateTime elevenFeb = new DateTime(2023, 02, 11, 13, 0, 0);
-            DateTime tenthFeb = new DateTime(2023, 02, 11, 15, 0, 0);
+            DateTime oneHourMeeting = new DateTime(2023, 02, 14, 10, 0, 0);
+            DateTime twoHoursMeeting = new DateTime(2023, 02, 14, 13, 0, 0);
             
             //skapar ett booking obj med hjälp av CreateBooking funktionen
             inputHandler.CreateBooking(bookID++, DateTime.Now, DateTime.Now.AddHours(2), "Survey", userOne);
             inputHandler.CreateBooking(bookID++, DateTime.Now.AddDays(1), DateTime.Now.AddDays(1).AddHours(3), "Conference", userTwo);
 
-            inputHandler.CreateBooking(bookID++, elevenFeb, elevenFeb.AddHours(2), "EC Meeting", userOne);
-            inputHandler.CreateBooking(bookID++, tenthFeb, tenthFeb.AddHours(2), "Hemtenta", userTwo);
+            inputHandler.CreateBooking(bookID++, oneHourMeeting, oneHourMeeting.AddHours(1), "EC Meeting", userOne);
+            inputHandler.CreateBooking(bookID++, twoHoursMeeting, twoHoursMeeting.AddHours(2), "Hemtenta", userTwo);
             //Deletes booking med id
             inputHandler.CancelBooking(1);
 
